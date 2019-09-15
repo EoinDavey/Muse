e3d2d4a5b5b2916b62cb79555f37e473237adbcf
Killian Kelly <killiank360@gmail.com>
2019-09-15 06:10:27 +0100

    clio :)

--- Clio/src/app/app.module.ts
+++ Clio/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {CodemirrorModule} from '@ctrl/ngx-codemirror';
+import {FormsModule} from '@angular/forms';
+import {HomeComponent} from './home/home.component';
+import {CoreComponent} from './editor/core/core.component';
+import { EditorComponent } from './editor/editor.component';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {HotkeyModule} from 'angular2-hotkeys';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoreComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CodemirrorModule,
+    HttpClientModule,
+    FormsModule,
+    HotkeyModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', pathMatch: 'full', component: HomeComponent},
+      { path: 'editor', pathMatch: 'full', component: EditorComponent},
+      { path: '**', pathMatch: 'full', redirectTo: '/home'},
+      { path: '', pathMatch: 'full', redirectTo: '/home'}
+    ])
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

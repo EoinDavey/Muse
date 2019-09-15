633324e0d99371e103c59d3876d4eee3a24bfc0b
dmac <DaraMac@users.noreply.github.com>
2019-09-15 09:41:03 +0100

    Added test exmaples for generate_audio

--- Erato/audio_engine.py
+++ Erato/audio_engine.py
@@ -1,17 +1,6 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
-#maths = AudioSegment.from_mp3("maths.mp3")
-#bushtit = AudioSegment.from_mp3("bushtit.mp3")
-
-# some hardcoded names of audio files for personal testing
-#sound_files = ["bell.mp3", "poker.mp3", "ice.mp3"]
-
-# corresponding keys by index for the sound files above
-#names = ["bell", "poker", "ice"]
-
-# this is what the dict construction will look for for nullary funcs
-# probably anyways
 #for i, name in enumerate(names):
 #    instrument[name] = (lambda local_i: (lambda: AudioSegment.from_file(sound_files[local_i],
 #        format="mp3")))(i)
@@ -21,15 +10,24 @@ from pydub.playback import play
 
 # might construct this programmatically later
 # if we have a greater range
-octave = ["C{}", "Db{}", "D{}", "Eb{}", "E{}", "F{}", "Gb{}", "G{}", "Ab{}", "A{}", "Bb{}", "B{}"]
+# octave = ["C{}", "Db{}", "D{}", "Eb{}", "E{}", "F{}", "Gb{}", "G{}", "Ab{}", "A{}", "Bb{}", "B{}"]
 
 
+# this is in milliseconds
+duration = 550
 instrument = dict()
 piano_keys = dict()
 
-for note in octave:
-    resolved_note = note.format("4")
-    piano_notes[note] = get_audio_segment_for_note("piano",resolved_note)
+hello_world = [
+        [('piano', 'C4'), ('piano', 'G4'), ('piano', 'D4')],
+        [('piano', 'E4'), ('piano', 'B4'), ('piano', 'Gb4')],
+        [('piano', 'G4'), ('piano', 'D4'), ('piano', 'A4')]
+        ]
+hello_barbie_lets_go_party = [
+        [('piano', 'C4'), ('piano', 'G4'), ('piano', 'D4')],
+        [('piano', 'E4'), ('piano', 'B4'), ('piano', 'Gb4')],
+        [('wait',), ('wait',), ('piano', 'A4')]
+ ]
 
 
 def get_audio_segment_for_note(instrument, note):
@@ -44,8 +42,11 @@ def drum(vol):
 def guitar(note, vol):
     pass
 
-instrument["piano"] = piano
+def wait():
+    return AudioSegment.silent(duration=duration)
 
+instrument["piano"] = piano
+instrument["wait"] = wait
 
 def pitch_shift(shift_octave, sound):
     new_sample_rate = int(sound.frame_rate * (2.0 ** shift_octave))
